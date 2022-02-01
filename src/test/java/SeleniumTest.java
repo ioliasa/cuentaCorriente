ce012355a8ecac2060e58ac80614a894a85bfba0
Inma <ioliasa@gmail.com>
2022-02-01 18:07:04 +0100

    Añadiendo Selenium otra vez

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -9,48 +9,47 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 public class SeleniumTest {
 
 	
-//	private WebDriver driver;
-//	
-//	@Before
-//	public void inicial() {
-//		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver");
-//		
-//		driver = new ChromeDriver();
-//	}
-//	
+	private WebDriver driver;
+	
+	@Before
+	public void inicial() {
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver");
+		
+		driver = new ChromeDriver();
+	}
+	
 	@Test
 	public void Pantalla() throws InterruptedException {
 		
-		assertTrue(true);
+		//assertTrue(true);
 		
-//		driver.get("http://localhost:8081/CuentaCorriente-0.0.1-SNAPSHOT/");
-//		
-//		WebElement ncc = driver.findElement(By.id("ncc"));
-//		WebElement saldo = driver.findElement(By.id("saldo"));
-//		WebElement ok = driver.findElement(By.id("ok"));
-//		Thread.sleep(4000);
-//		ncc.sendKeys("12345678");
-//		Thread.sleep(4000);
-//
-//		saldo.sendKeys("1000");
-//		Thread.sleep(4000);
-//
-//		ok.click();
-//		Thread.sleep(4000);
-//
-//		String titulo = driver.getCurrentUrl();
-//		
-//		assertTrue(titulo.equals("http://localhost:8081/CuentaCorriente-0.0.1-SNAPSHOT/adminBanco?action=empezar"));
+		driver.get("http://localhost:8081/CuentaCorriente-0.0.1-SNAPSHOT/");
+		
+		WebElement ncc = driver.findElement(By.id("ncc"));
+		WebElement saldo = driver.findElement(By.id("saldo"));
+		WebElement ok = driver.findElement(By.id("ok"));
+		//Thread.sleep(4000);
+		ncc.sendKeys("12345678");
+		//Thread.sleep(4000);
+
+		saldo.sendKeys("1000");
+		//Thread.sleep(4000);
+
+		ok.click();
+		//Thread.sleep(4000);
+
+		String titulo = driver.getCurrentUrl();
+		
+		assertTrue(titulo.equals("http://localhost:8081/CuentaCorriente-0.0.1-SNAPSHOT/adminBanco?action=empezar"));
 		
 }
 
-//	@After
-//	public void shutdown() {
-//		driver.quit();
-//	}
+	@After
+	public void shutdown() {
+		driver.quit();
+	}
 }
